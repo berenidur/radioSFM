274503c0f58e6dd37584be42a62775e4d8787a6a
berenidur <rodolfohuacasi@gmail.com>
2026-09-01 23:17:18 -0500

    parfor

--- sfm2_bscs_cases_2x2avg.m
+++ sfm2_bscs_cases_2x2avg.m
@@ -333,7 +333,7 @@ for caseIdx = 1:size(cases, 1)
             params_valid = nan(nValid, 11);
 
 
-            for k = 1:nValid
+            parfor k = 1:nValid
 
                 x = rows(k);
                 y = cols(k);
